db4e91fdfd3002ec8f3e631d3348890c3caa4a60
AngeloBastos <angelohbastos@gmail.com>
2021-06-09 15:59:36 -0300

    feat: gerencia a tabela verdade

--- js/TruthTable.js
+++ js/TruthTable.js
@@ -1,14 +1,17 @@
 export default class TruthTable {
-  constructor(cell, typeMap, permutation) {
-    this.cell = cell
+  constructor(squares, typeMap, permutation) {
+    this.squares = squares
     this.typeMap = typeMap
     this.permutation = permutation
+    this.render()
   }
   render() {
-    return `
-        ${new TableHead(this.typeMap).render()}
-        ${new Permutation(this.typeMap, this.permutation).render()}
-  `
+    const table = document.querySelector('#truthTable')
+    table.innerHTML = `
+      ${new TableHead(this.typeMap).render()}
+      ${new Permutation(this.typeMap, this.permutation).render()}
+    `
+    new TableValSelection(this.squares, this.typeMap)
   }
 }
 
@@ -26,7 +29,7 @@ class Permutation {
     for (let i = 0; i < typeMap; i++) {
       rowCols.push(this.renderCols(permutation[index][i]))
     }
-    rowCols.push(this.renderCols(`<button class="btn btn-cell" value="0">0</button>`))
+    // rowCols.push(this.renderCols(`<button class="btn btn-cell" value="0">0</button>`))
     return rowCols.join('')
   }
   renderTableRow (permutation, index) {
@@ -65,5 +68,57 @@ class TableHead {
   }
   render () {
     return `<thead><tr>${this.tableRow()}</tr></thead>`
+  }
+}
+
+class TableValSelection {
+  constructor (squares, typeMap) {
+    this.squares = squares
+    this.typeMap = typeMap
+    this.renderTableCol()
+  }
+
+  renderButton (i, j) {
+    return `
+      <button
+        class="btn btn-cell"
+        data-celltab="${"" + i + j}"
+      >${this.squares[i][j][0]}</button>`
+  }
+  renderTableCol () {
+    let rolsTable = document.querySelectorAll('#truthTable > tbody > tr')
+    let a = []
+    let r = this.typeMap
+    let c = this.typeMap
+
+    if (this.typeMap === 3) {
+      r = 2
+      c = 4
+    }
+
+    for (let i = 0; i < c; i++) {
+      let l
+      if (i === 2) {
+        l = 3
+      } else if (i === 3) {
+        l = 2
+      } else {
+        l = i
+      }
+      for (let j = 0; j < r; j++) {
+        let k
+        if (j % r === 2) {
+          k = 3
+        } else if (j % r === 3) {
+          k = 2
+        } else {
+          k = j
+        }
+        a.push(this.renderButton(k, l))
+      }
+    }
+    for (let i = 0; i < 2 ** this.typeMap; i++) {
+      rolsTable[i].innerHTML += `<td>${a[i]}</td>`
+    }
   }
 }
